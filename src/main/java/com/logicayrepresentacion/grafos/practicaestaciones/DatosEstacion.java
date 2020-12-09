15e13f50834f0538d60ca3ac731b6b0e253b8e99
carlosmding <carlosaph@gmail.com>
2020-12-09 18:21:39 -0500

    Toda practica

---
+++ src/main/java/com/logicayrepresentacion/grafos/practicaestaciones/DatosEstacion.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.logicayrepresentacion.grafos.practicaestaciones;
+
+import arbol.binario.listaligada.busqueda.avl.ArbolAVL;
+import arbol.binario.listaligada.busqueda.avl.NodoAVL;
+
+/**
+ *
+ * @author 57300
+ */
+public class DatosEstacion {
+
+    Estacion[] estaciones;
+    ArbolAVL<Estacion> arbolAVL = new ArbolAVL();
+    private static int SIGUIENTE_ID = 0;
+    private Grafo grafo;
+
+    public DatosEstacion(int c) {
+        SIGUIENTE_ID = 0;
+        estaciones = new Estacion[c];
+        grafo = new Grafo(c);
+    }
+
+    public Estacion add(String ciudad1) throws CantidadEstacionesException {
+
+        NodoAVL<Estacion> nodo = arbolAVL.buscar(new Estacion(ciudad1, -1));
+        if (nodo == null) {
+            int siguienteID = DatosEstacion.nextSIGUIENTE_ID();
+            if (siguienteID >= estaciones.length) {
+                throw new CantidadEstacionesException("Sobrepaso la cantidad de estaciones");
+            }
+            Estacion estacion = new Estacion(ciudad1, siguienteID);
+            arbolAVL.insertarDato(estacion);
+            estaciones[estacion.getId()] = estacion;
+            return estacion;
+        } else {
+            return (Estacion) nodo.getDato();
+        }
+    }
+
+    public static int nextSIGUIENTE_ID() {
+        int actual = SIGUIENTE_ID;
+        SIGUIENTE_ID++;
+        return actual;
+    }
+
+    public static int getSIGUIENTE_ID() {
+        return SIGUIENTE_ID;
+    }
+
+    public Estacion[] getEstaciones() {
+        return estaciones;
+    }
+
+    public Estacion buscar(String ciudad1) {
+        NodoAVL<Estacion> nodo = arbolAVL.buscar(new Estacion(ciudad1, -1));
+        if (nodo == null) {
+            return null;
+        } else {
+            return (Estacion) nodo.getDato();
+        }
+    }
+
+    public void addAdyacencia(String ciudad1, String ciudad2, int distancia) throws CantidadEstacionesException {
+        Estacion estacion1 = add(ciudad1);
+        Estacion estacion2 = add(ciudad2);
+        grafo.addAdyancencia(estacion1.getId(), estacion2.getId(), distancia);
+    }
+
+    public Grafo getGrafo() {
+        return grafo;
+    }
+    
+    
+
+}
---
+++ src/main/java/com/logicayrepresentacion/grafos/practicaestaciones/Grafo.java
@@ -0,0 +1,264 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.logicayrepresentacion.grafos.practicaestaciones;
+
+/**
+ *
+ * @author 57300
+ */
+public class Grafo {
+
+    private int[][] matrizAdy;
+    private int[][] matrizCostos;
+
+    public Grafo(int cantidadEstaciones) {
+        matrizAdy = new int[cantidadEstaciones][cantidadEstaciones];
+        matrizCostos = new int[cantidadEstaciones][cantidadEstaciones];
+    }
+
+    public int[][] getMatrizCostos() {
+        return matrizCostos;
+    }
+
+    public int[][] getMatrizAdy() {
+        return matrizAdy;
+    }
+
+    public void addAdyancencia(int vi, int vj, int distancia) {
+        matrizAdy[vi][vj] = 1;
+        matrizAdy[vj][vi] = 1;
+        matrizCostos[vi][vj] = distancia;
+        matrizCostos[vj][vi] = distancia;
+    }
+
+    public Ruta obtenerRuta(int vi, int vj) {
+        Ruta ruta = new Ruta();
+
+        Costo[][] costos = new Costo[matrizCostos.length][matrizCostos.length];
+        for (int i = 0; i < matrizCostos.length; i++) {
+            for (int j = 0; j < matrizCostos.length; j++) {
+                if (matrizAdy[i][j] == 0) {
+                    costos[i][j] = new Costo();
+                } else {
+                    costos[i][j] = new Costo(matrizCostos[i][j]);
+                }
+            }
+        }
+
+        int[] visitados = new int[matrizCostos.length];
+        Costo[] costoMinimo = new Costo[matrizCostos.length];
+        for (int j = 0; j < matrizCostos.length; j++) {
+            costoMinimo[j] = costos[vi][j];
+        }
+
+        visitados[vi] = 1;
+        int indice = 0;
+        while (indice < matrizCostos.length - 1) {
+            int w = escogerMenor(costoMinimo, visitados);
+            visitados[w] = 1;
+            indice++;
+            for (int j = 0; j < visitados.length; j++) {
+                if (visitados[j] == 0) {
+                    Costo costo1 = costoMinimo[j];
+                    Costo distancia = costos[w][j];
+                    Costo costo2 = Costo.sumar(costoMinimo[w], distancia);
+                    costoMinimo[j] = Costo.menor(costo1, costo2);
+                }
+            }
+        }
+
+        return ruta;
+    }
+
+    private int escogerMenor(Costo[] costoMinimo, int[] visitados) {
+        int w = 0;
+        Costo minimow = costoMinimo[w];
+        for (int j = 0; j < visitados.length; j++) {
+            if (visitados[j] == 0) {
+                Costo posiblemenor = Costo.menor(minimow, costoMinimo[j]);
+                if (posiblemenor != minimow) {
+                    minimow = posiblemenor;
+                    w = j;
+                }
+            }
+        }
+        return w;
+    }
+
+    public Costo[][] convertirMatriz() {
+        Costo[][] matriz = new Costo[matrizCostos.length][matrizCostos.length];
+        for (int i = 0; i < matrizCostos.length; i++) {
+            for (int j = 0; j < matrizCostos.length; j++) {
+                if (matrizAdy[i][j] == 1) {
+                    matriz[i][j] = new Costo(matrizCostos[i][j]);
+                } else {
+                    matriz[i][j] = new Costo();
+                }
+            }
+        }
+        return matriz;
+    }
+
+    public Costo[][] menoresCostos(Costo[][] costos) {
+        int n = costos.length;
+        Costo[][] menoresCostos = new Costo[n][n];
+        for (int i = 0; i < n; i++) {
+            System.arraycopy(costos[i], 0, menoresCostos[i], 0, n);
+        }
+
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (i != j) {
+                        Costo aux = Costo.sumar(menoresCostos[i][k], menoresCostos[k][j]);
+                        switch (aux.compareTo(menoresCostos[i][j])) {
+                            case 1:
+                                break;
+                            case -1:
+
+                            case 0:
+                                //System.out.println("acá");
+                                if (i != j) {
+                                    menoresCostos[i][j] = aux;
+                                    break;
+                                }
+
+                        }
+                    }
+
+                }
+            }
+        }
+        return menoresCostos;
+    }
+
+    public int[][] enviarMensaje(Costo[][] menores, int estInicio, int lados) {
+        int[] menoresXEstacion = new int[menores.length];
+        int[] estEnviadas = new int[menores.length];
+        int[] adyEstacion = new int[menores.length];
+        int[][] ruta = new int[lados][2];
+        int contadorRutas = 0;
+
+        estEnviadas[estInicio] = -1;
+
+        //menores de estacion de inicio del mensaje
+        for (int i = 0; i < menores.length; i++) {
+            menoresXEstacion[i] = menores[estInicio][i].getValor();
+            adyEstacion[i] = matrizAdy[estInicio][i];
+        }
+
+        //Enviados desde el inicio
+        for (int i = 0; i < adyEstacion.length; i++) {
+            if (adyEstacion[i] == 1) {
+                if (menoresXEstacion[i] == matrizCostos[estInicio][i] && estInicio != i && estEnviadas[i] == 0) {
+                    System.out.println("La estacion " + estInicio + " envia mensaje a la estacion " + i);
+                    ruta[contadorRutas][0] = estInicio;
+                    ruta[contadorRutas][1] = i;
+                    contadorRutas++;
+                    estEnviadas[i] = 1;
+                }
+            }
+        }
+
+        //Enviar desde estaciones que ya tiene mensaje
+        boolean finaliza = mensajeTransmitido(estEnviadas);
+        while (finaliza) {
+            for (int i = 0; i < menores.length; i++) {
+                if (estEnviadas[i] == 1) {
+                    for (int j = 0; j < menores.length; j++) {
+                        adyEstacion[j] = matrizAdy[i][j];
+                    }
+
+                    for (int k = 0; k < menores.length; k++) {
+                        if (adyEstacion[k] == 1) {
+                            if (estEnviadas[k] == 0) {
+                                int menorCosto;
+                                if (k == 0) {
+                                    menorCosto = 1000;
+                                } else {
+                                    menorCosto = menores[0][k].getValor();
+                                }
+
+                                for (int j = 1; j < menores.length; j++) {
+                                    if (menores[j][k].getValor() < menorCosto && k != j) {
+                                        menorCosto = menores[j][k].getValor();
+                                    }
+                                }
+                                if (menorCosto == matrizCostos[i][k]) {
+                                    System.out.println("La estacion " + i + " envia mensaje a la estacion " + k);
+                                    ruta[contadorRutas][0] = i;
+                                    ruta[contadorRutas][1] = k;
+                                    contadorRutas++;
+                                    estEnviadas[k] = 1;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            finaliza = mensajeTransmitido(estEnviadas);
+        }
+
+        return ruta;
+    }
+
+    public boolean mensajeTransmitido(int[] estaciones) {
+        boolean mensajePendiente = false;
+        for (int i = 0; i < estaciones.length; i++) {
+            if (estaciones[i] == 0) {
+                mensajePendiente = true;
+            }
+        }
+        return mensajePendiente;
+    }
+
+    public String rutaCorta(Costo[][] menores, int vi, int vf) {
+        //Ruta camino = new Ruta();
+        String camino = "";
+        if (matrizAdy[vf][vi] != 1) {
+            int[] visitados = new int[menores.length];
+            int[] adyEstacion = new int[menores.length];
+            camino += vf;
+            int sigEstacion = -1;
+
+            visitados[vi] = -1;
+            visitados[vf] = -1;
+
+            for (int i = 0; i < visitados.length; i++) {
+                int menorCosto = 1000;
+                if (!menores[i][vf].isInfinito() && menores[i][vf].getValor() < menorCosto) {
+                    menorCosto = menores[i][vf].getValor();
+                    sigEstacion = i;
+                }   
+            }
+            camino += ";" + sigEstacion;
+            visitados[sigEstacion] = 1;
+            
+            boolean finaliza = mensajeTransmitido(visitados);
+            while (finaliza) {
+                if (matrizAdy[sigEstacion][vi]!=1) {
+                    for (int i = 0; i < visitados.length; i++) {
+                        int menorCosto = 100; 
+                        if (!menores[i][sigEstacion].isInfinito() && menores[i][sigEstacion].getValor() < menorCosto && visitados[sigEstacion]==0) {
+                            menorCosto = menores[i][vf].getValor();
+                            sigEstacion = i;
+                        }   
+                    }
+                    visitados[sigEstacion] = 1;
+                    camino += ";" + sigEstacion;   
+                }else{
+                    camino+=";" + sigEstacion;
+                    break;
+                }
+                finaliza = mensajeTransmitido(visitados);
+            }   
+        } else {
+            camino += ";" + vi;
+        }
+        return camino;
+    }
+
+}
